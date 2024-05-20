c9c5ad5474533edb18cbbc7dc1ca1068ed386d5b
Olatoye-Daramola <daramolatoye@gmail.com>
2024-05-20 08:54:11 +0100

    dev: add configurations for lambda deployment

--- userManagementService/src/main/java/dev/olatoye/userManagementService/service/UserService.java
+++ userManagementService/src/main/java/dev/olatoye/userManagementService/service/UserService.java
@@ -1,19 +1,21 @@
 package dev.olatoye.userManagementService.service;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import dev.olatoye.userManagementService.model.dto.request.CreateUserDto;
 import dev.olatoye.userManagementService.model.entity.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -21,13 +23,18 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class UserService {
 
+    @Value("${cloud.aws.notification.queue}")
+    private String notificationQueue;
+
     private final DynamoDBMapper dynamoDBMapper;
+    private final QueueMessagingTemplate queueMessagingTemplate;
     private final ObjectMapper objectMapper;
     private final Logger LOG = LoggerFactory.getLogger(UserService.class);
     Set<String> processedMessages = ConcurrentHashMap.newKeySet();
 
-    public UserService(DynamoDBMapper dynamoDBMapper, ObjectMapper objectMapper) {
+    public UserService(DynamoDBMapper dynamoDBMapper, QueueMessagingTemplate queueMessagingTemplate, ObjectMapper objectMapper) {
         this.dynamoDBMapper = dynamoDBMapper;
+        this.queueMessagingTemplate = queueMessagingTemplate;
         this.objectMapper = objectMapper;
     }
 
@@ -38,6 +45,7 @@ public class UserService {
         if (!isAdded) return;
         CreateUserDto dto = new Gson().fromJson(message, CreateUserDto.class);
         boolean savedCustomer = save(dto);
+        sendNotificationToQueue(dto.getEmail());
     }
 
     private boolean save(CreateUserDto customerToSave) {
@@ -49,5 +57,11 @@ public class UserService {
         } catch (Exception e) {
             return false;
         }
+    }
+
+    private void sendNotificationToQueue(String email) {
+        Message<String> message = MessageBuilder.withPayload("Your account has been created. You can proceed with transacting" +
+                " with your account.").build();
+        queueMessagingTemplate.send(email, message);
     }
 }
--- walletService/src/main/java/dev/olatoye/walletservice/service/TransactionService.java
+++ walletService/src/main/java/dev/olatoye/walletservice/service/TransactionService.java
@@ -8,9 +8,13 @@ import dev.olatoye.walletservice.model.entity.Transaction;
 import dev.olatoye.walletservice.model.entity.Wallet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -21,15 +25,20 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class TransactionService {
 
+    @Value("${cloud.aws.notification.queue}")
+    private String notificationQueue;
+
     private final WalletService walletService;
     private final DynamoDBMapper dynamoDBMapper;
+    private final QueueMessagingTemplate queueMessagingTemplate;
     private final ObjectMapper objectMapper;
     private final Logger LOG = LoggerFactory.getLogger(TransactionService.class);
     Set<String> processedMessages = ConcurrentHashMap.newKeySet();
 
-    public TransactionService(WalletService walletService, DynamoDBMapper dynamoDBMapper, ObjectMapper objectMapper) {
+    public TransactionService(WalletService walletService, DynamoDBMapper dynamoDBMapper, QueueMessagingTemplate queueMessagingTemplate, ObjectMapper objectMapper) {
         this.walletService = walletService;
         this.dynamoDBMapper = dynamoDBMapper;
+        this.queueMessagingTemplate = queueMessagingTemplate;
         this.objectMapper = objectMapper;
     }
 
@@ -41,30 +50,17 @@ public class TransactionService {
 
         InitiateTransactionRequest txnRequest = new Gson().fromJson(message, InitiateTransactionRequest.class);
         boolean registeredTxn = save(txnRequest);
+        //Pull customer email from db
+        //sendNotificationToQueue(customer.getEmail());
     }
 
     private boolean save(InitiateTransactionRequest txnRequest) {
         try {
             //Handle Transaction creation
-            Transaction txn = objectMapper.convertValue(txnRequest, Transaction.class);
-            txn.setVat(0.5);
-            BigDecimal vatAmount = txn.getAmount().multiply(BigDecimal.valueOf(txn.getVat())).setScale(2, RoundingMode.HALF_EVEN);
-            BigDecimal settledAmount = txn.getAmount().add(vatAmount);
-            txn.setSettledAmount(settledAmount.setScale(2, RoundingMode.HALF_EVEN));
-
-            //Update wallet position
+            Transaction txn = createTxnObject(txnRequest);
+            //Validate transaction with wallet position
             Wallet wallet = walletService.findByOwnerUniqueId(txn.getDestinationUniqueId());
-            if (!txn.isCreditTransaction()) {
-                int isTxnAmountMoreThanAvailableBalance = wallet.getBalance().compareTo(txn.getSettledAmount());
-                if (isTxnAmountMoreThanAvailableBalance < 0) return false;
-                wallet.setBalance(wallet.getBalance().subtract(txn.getSettledAmount()));
-            }
-            else {
-                int isTxnAmountMoreThanAllowedCreditLimit = wallet.getAmountLimit().compareTo(txn.getSettledAmount());
-                if (isTxnAmountMoreThanAllowedCreditLimit < 0) return false;
-                wallet.setBalance(wallet.getBalance().add(txn.getSettledAmount()));
-            }
-
+            if (!isValidTransaction(txn, wallet)) return false;
             //Save transaction instruments
             txn.setSuccessful(true);
             dynamoDBMapper.save(txn);
@@ -74,5 +70,35 @@ public class TransactionService {
             // Rollback saved details
             return false;
         }
+    }
+
+    private Transaction createTxnObject(InitiateTransactionRequest txnRequest) {
+        Transaction txn = objectMapper.convertValue(txnRequest, Transaction.class);
+        txn.setVat(0.5);
+        BigDecimal vatAmount = txn.getAmount().multiply(BigDecimal.valueOf(txn.getVat())).setScale(2, RoundingMode.HALF_EVEN);
+        BigDecimal settledAmount = txn.isCreditTransaction()
+                ? txn.getAmount().subtract(vatAmount) : txn.getAmount().add(vatAmount);
+        txn.setSettledAmount(settledAmount.setScale(2, RoundingMode.HALF_EVEN));
+        return txn;
+    }
+
+    private boolean isValidTransaction(Transaction txn, Wallet wallet) {
+        if (!txn.isCreditTransaction()) {
+            int isTxnAmountMoreThanAvailableBalance = wallet.getBalance().compareTo(txn.getSettledAmount());
+            if (isTxnAmountMoreThanAvailableBalance < 0) return false;
+            wallet.setBalance(wallet.getBalance().subtract(txn.getSettledAmount()));
+        }
+        else {
+            int isTxnAmountMoreThanAllowedCreditLimit = wallet.getAmountLimit().compareTo(txn.getSettledAmount());
+            if (isTxnAmountMoreThanAllowedCreditLimit < 0) return false;
+            wallet.setBalance(wallet.getBalance().add(txn.getSettledAmount()));
+        }
+        return true;
+    }
+
+    private void sendNotificationToQueue(String email) {
+        Message<String> message = MessageBuilder.withPayload("Your account has been created. You can proceed with transacting" +
+                " with your account.").build();
+        queueMessagingTemplate.send(email, message);
     }
 }
